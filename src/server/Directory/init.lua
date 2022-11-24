f60072696020cc4589cb4c191f26c30f007a9edf
Commandbot2 <mathisdylan6@yahoo.com>
2022-11-24 02:00:07 -0600

    Backup

--- src/server/Directory/init.lua
+++ src/server/Directory/init.lua
@@ -1,47 +1,12 @@
+local Players = game:GetService("Players")
 local module = {}
 
-local setting = {
 
-	["NightLighting"] = {
-
-		Disabled = true,
-
-		["0"] = function()
-			local n = game.Lighting 
-			n.ClockTime = 4
-			n.EnvironmentDiffuseScale = 0
-			n.EnvironmentSpecularScale = 0
-			n.Brightness = 1.15
-		end
-	},
-
-	["DayLighting"] = {
-
-		Disabled = false,
-
-		["0"] = function()
-			local n = game.Lighting 
-			n.ClockTime = 8.1
-			n.EnvironmentDiffuseScale = 0
-			n.EnvironmentSpecularScale = 0
-			n.Brightness = 2.47
-		end
-	}
-
-}
-
-for i, v in pairs(setting) do
-	if v.Disabled == true then continue end
-	v["0"]()
-	for i, v in pairs(game.Lighting.Temp:GetChildren()) do
-		v.Parent = game.Lighting
+function module:GetModule(module, dir)
+	local v = script.Parent:FindFirstChild(dir)
+	if v:IsA("ModuleScript") and v.Name == module then
+		return v
 	end
-	game.Lighting.Temp:Destroy()
 end
-
-function module:generateWater()
-	
-end
-
 
 return module
